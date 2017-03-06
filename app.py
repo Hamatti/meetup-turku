8b8718ede87acae11609c4c04674b6cd6d007de7
Juha-Matti Santala <juhamattisantala@gmail.com>
2017-03-06 18:08:31 +0100

    Fix app.py running settings

--- app.py
+++ app.py
@@ -14,5 +14,6 @@ def hello():
         meetups.append(mt)
     return render_template('index.html', meetups=meetups)
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
